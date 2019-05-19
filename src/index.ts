5fca7fde3e8ce67222b9cb05ddb4de4092cac40e
Blake Embrey <hello@blakeembrey.com>
2019-05-18 23:02:16 -0700

    Export class instance types alongside constructors

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,11 @@ import {
 export * from "./headers";
 export * from "./signal";
 
+export type Body = NodeBody | BrowserBody;
 export const Body: typeof NodeBody | BrowserBody = NodeBody;
+export type Request = NodeRequest | BrowserRequest;
 export const Request: typeof NodeRequest | typeof BrowserRequest = NodeRequest;
+export type Response = NodeResponse | BrowserResponse;
 export const Response:
   | typeof NodeResponse
   | typeof BrowserResponse = NodeResponse;
