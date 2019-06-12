525c1cea4e6bdf17938c8f00c717cd6dd1b24a35
Blake Embrey <hello@blakeembrey.com>
2019-06-11 22:26:12 -0700

    Use `@servie/events`

--- src/signal.ts
+++ src/signal.ts
@@ -1,66 +1,4 @@
-/**
- * Event listener type.
- */
-type EventListener<T, K extends keyof T> = (
-  ...args: (T & Record<PropertyKey, any[]>)[K]
-) => void;
-
-/**
- * Wildcard event listener type.
- */
-type EachEventListener<T> = {
-  [K in keyof T]: (
-    type: K,
-    ...args: (T & Record<PropertyKey, any[]>)[K]
-  ) => void
-}[keyof T];
-
-/**
- * Type-safe event emitter.
- */
-class Events<T> {
-  all: Array<EachEventListener<T>> = [];
-  any: { [K in keyof T]: Array<EventListener<T, K>> } = Object.create(null);
-
-  on<K extends keyof T>(type: K, callback: EventListener<T, K>) {
-    (this.any[type] = this.any[type] || []).push(callback);
-  }
-
-  off<K extends keyof T>(type: K, callback: EventListener<T, K>) {
-    const stack = this.any[type] || [];
-    stack.splice(stack.indexOf(callback) >>> 0, 1);
-  }
-
-  each(callback: EachEventListener<T>) {
-    this.all.push(callback);
-  }
-
-  none(callback: EachEventListener<T>) {
-    this.all.splice(this.all.indexOf(callback) >>> 0, 1);
-  }
-
-  emit<K extends keyof T>(
-    type: K,
-    ...args: (T & Record<PropertyKey, any[]>)[K]
-  ) {
-    (this.any[type] || []).slice().forEach(fn => fn(...args));
-    this.all.slice().forEach(fn => fn(type, ...args));
-  }
-}
-
-/**
- * Helper to listen to an event once only.
- */
-function once<T, K extends keyof T>(
-  e: Events<T>,
-  type: K,
-  callback: EventListener<T, K>
-) {
-  return e.on(type, function once(...args) {
-    e.off(type, once);
-    callback(...args);
-  });
-}
+import { Emitter } from "@servie/events";
 
 /**
  * Dictionary of supported signal events.
@@ -78,14 +16,14 @@ export interface SignalEvents {
 /**
  * Standard signal used to communicate during `request` processing.
  */
-export class Signal extends Events<SignalEvents> {
+export class Signal extends Emitter<SignalEvents> {
   aborted = false;
 
   constructor() {
     super();
 
     // Listen for the abort signal.
-    once(this, "abort", () => (this.aborted = true));
+    this.on("abort", () => (this.aborted = true));
   }
 }
 
